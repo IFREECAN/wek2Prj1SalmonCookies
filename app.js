6926b5da77d8410723583ad13282c526f9edde36
IFREECAN <jasper.kinnay@gmmail.com>
2017-12-11 19:04:52 -0800

    added function to give the average number of cookies sold at each store...next part is how to apply this for each hour for each store??

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 //hours the store is opened... may be used to for each store hours since all stores operates during the same times//
-var storeOpenedTimes = {'06:00am', '07:00am', '08:00am', '09:00am', '10:00am', '11:00am', '12:00pm', '01:00PM', '02:00PM',
-  '03:00PM', '04:00PM', '05:00PM', '06:00PM', '07:00PM', '08:00PM'}
+var storeOpenedTimes = ['06:00am', '07:00am', '08:00am', '09:00am', '10:00am', '11:00am', '12:00pm', '01:00PM', '02:00PM',
+  '03:00PM', '04:00PM', '05:00PM', '06:00PM', '07:00PM', '08:00PM']
 
 var pikeStore = {
   minCustomerPerHour: 23,
   maxCustomerPerHour: 65,
-  AvgCPH: 6.3,
+  avgCookiesSoldPerHour: 6.3,
   storeName: 'pikeStore',
   //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
-  productSoldPerHour: [16, 20, 35, 48, 56, 77, 93, 144, 119, 84, 61, 23, 42, 57, 29, 29],
+  productSoldPerHour: [],
 }
 
 var seaTacAirportStore = {
   minCustomerPerHour: 3,
   maxCustomerPerHour: 24,
-  AvgCPH: 1.2,
+  avgCookiesSoldPerHour: 1.2,
   storeName: 'seaTacAirportStore',
   //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
   productSoldPerHour: [],
@@ -23,7 +23,7 @@ var seaTacAirportStore = {
 var seattleCenterStore = {
   minCustomerPerHour: 11,
   maxCustomerPerHour: 38,
-  AvgCPH: 3.7,
+  avgCookiesSoldPerHour: 3.7,
   storeName: 'seattleCenterStore',
   //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
   productSoldPerHour: [],
@@ -32,21 +32,39 @@ var seattleCenterStore = {
 var capitalHillStore = {
   minCustomerPerHour: 20,
   maxCustomerPerHour: 38,
-  AvgCPH: 2.3,
+  avgCookiesSoldPerHour: 2.3,
   storeName: 'capitalHillStore',
   //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
   productSoldPerHour: [],
+  averageCookiesPerHour: function () {
+    return Math.floor(Math.random()) * ((this.maxCustomerPerHour - this.minCustomerPerHour) + this.minCustomerPerHour));
+  }
 }
+console.log(capitalHillStore.averageCookiesPerHour())
 
 var alkiStore = {
   minCustomerPerHour: 2,
   maxCustomerPerHour: 16,
-  avgCustomerPerHour: 4.6,
+  avgCookiesSoldPerHour: 4.6,
   storeName: 'alkiStore',
   //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
   productSoldPerHour: [],
+  averageCustomerPerHour: function () {
+    return Math.floor(Math.random()) * ((this.maxCustomerPerHour - this.minCustomerPerHour) + this.minCustomerPerHour));
+  }
+
 }
 
-acturalCustomerPerHourCount: function(minCustomerPerHour, maxCustomerPerHour) {
-  return math.floor(math.random) * ((this.maxCustomerPerHour - this.minCustomerPerHour) + this.minCustomerPerHour));
+console.log(alkiStore.averageCookiesPerHour())
+
+/*
+acturalCustomerPerHourCount = function(store) {
+  return Math.floor(Math.random()) * ((store.maxCustomerPerHour - store.minCustomerPerHour) + store.minCustomerPerHour));
 }
+
+
+console.log(acturalCustomerPerHourCount(alkiStore))
+
+function averageCookiesPerHour () {
+  return
+} */
