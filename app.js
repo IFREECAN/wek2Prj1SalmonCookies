71a1caff0c8a85879333079614c644e91d23b8a2
IFREECAN <jasper.kinnay@gmmail.com>
2017-12-11 16:47:34 -0800

    created each stores min, max and avg.

---
+++ app.js
@@ -0,0 +1,44 @@
+var pikeStore = {
+  minCustomerPerHour: 23,
+  maxCustomerPerHour: 65,
+  AvgCPH: 6.3,
+  storeName: 'pikeStore',
+  //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
+  productSoldPerHour: [16, 20, 35, 48, 56, 77, 93, 144, 119, 84, 61, 23, 42, 57, 29, 29],
+}
+
+var seaTacAirportStore = {
+  minCustomerPerHour: 3,
+  maxCustomerPerHour: 24,
+  AvgCPH: 1.2,
+  storeName: 'seaTacAirportStore',
+  //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
+  productSoldPerHour: [],
+}
+
+var seattleCenterStore = {
+  minCustomerPerHour: 11,
+  maxCustomerPerHour: 38,
+  AvgCPH: 3.7,
+  storeName: 'seattleCenterStore',
+  //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
+  productSoldPerHour: [],
+}
+
+var capitalHillStore = {
+  minCustomerPerHour: 20,
+  maxCustomerPerHour: 38,
+  AvgCPH: 2.3,
+  storeName: 'capitalHillStore',
+  //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
+  productSoldPerHour: [],
+}
+
+var alkiStore = {
+  minCustomerPerHour: 2,
+  maxCustomerPerHour: 16,
+  avgCustomerPerHour: 4.6,
+  storeName: 'alkiStore',
+  //*zero=storeopening at 6:00am and lastvalue= closingtime from store opening which is 8:00PM//
+  productSoldPerHour: [],
+}
