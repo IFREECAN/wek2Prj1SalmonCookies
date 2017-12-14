321d42a47b0473c062c9a2ddaebbd33b17cd91e7
IFREECAN <jasper.kinnay@gmmail.com>
2017-12-14 14:08:54 -0800

    updates cleaning up codes

--- Day7SalmondCookies/appsUpDate1213.js
+++ Day7SalmondCookies/appsUpDate1213.js
@@ -6,11 +6,11 @@ var storeOpenedTimes = ['06:00am', '07:00am', '08:00am', '09:00am', '10:00am', '
 var salmonCookiesTable = document.getElementByID('salmon-cookies-table');
 
 //CREATE THE ARREY TO HOLD ALL STORES INFO
-var allSalmonCookiesStores = [];
+var allStores = [];
 
 //defind what the function random is:
 function random(min, max) {
-  return Math.floor(Math.random()) * ((max - min) + min));
+  return Math.floor(Math.random()) * ((max - min) + min)); // equation is fron MDN
 }
 //Constructor to make the table object
 function salmonCookiesStore(location, minCustomer, maxCustomer, avgCookiesPerCustomer, customerPerHour, cookiesSoldPerHour, totalDailySales) {
@@ -21,7 +21,7 @@ function salmonCookiesStore(location, minCustomer, maxCustomer, avgCookiesPerCus
   this.avgCustomersPerHour =  [];
   this.cookiesSoldPerHour =  [];
   this.totalDailySales = 0;
-  allSalmondCookiesStores.push(this);
+  allStores.push(this);
   this.calcCustomerPerHour = function() {
     for(var i = 0; i < hours.length; i++) {
       var avgCustomers = random(this.minCustsPerHour, this.maxCustsPerHour); //random # being saved to "avgCustomers" for each store.
@@ -40,7 +40,6 @@ this.totalDailySales = function(){
   this.totalDailySales += this.CookiesSoldPerHour[i]; //total daily sale = cookies sold per hour x total cookies sold at (each-hours = [i]) the store is opened
 
   }
-//COMMENT: CREATE A ROW FOR HEADER --> CREATE A LOOP THAT GOES THRHOUR EACH HOUR FOR EACH STORE......... 
 
 }
 }
@@ -52,23 +51,52 @@ new SalmondCookiesStore('seaTacAirportStore', 3, 24, 1.2);
 new SalmondCookiesStore('seattleCenterStore', 11, 38, 3.7);
 new SalmondCookiesStore('capitalHillStore', 20, 38, 2.3);
 new SalmondCookiesStore('alkiStore', 2, 16, 4.6);
+//could also be written as: var intro = new SalmondCookiesStore('alkiStore', 2, 16, 4.6);
+
+
+//************************************************************************
+//COMMENT: CREATE A ROW FOR HEADER --> CREATE A LOOP THAT GOES THRHOUR EACH
+//HOUR FOR EACH STORE.........
+//**************************************************************************
+
+// NEW::12-14-17
+    //NEW::12-14-17 CREATE THE 'RENTER' FUNCTIONS
+
+render = function(){
+  this.calcCookiesSoldPerHour();
 
     //CREATE TABLE-ROW
-    var row = document.createElement('tr');
+    var rows  = document.createElement('tr'); //noet: rows=tableRowElement
     //next: create table data
-    var column = document.createElement('td');
-    //add content to tabe data (name of each store)
-    tdElement.textContent = this.location;
+    var columns = document.createElement('th'); //noet: rows=tablecolumnElement
+    //add content to tabe header (which in this case is the name  of each store)
+    columnsElement.textContent = this.location;
     //append the table data
-    trElement.appendChild(tdElement);
+    rowsElement.appendChild(columnsElement);
 
+    //NEW12-13:: CREATE TABLE-data --> add contents to them, append for hourly numbers
+    for(avar i = 0; hours.length; i++){
+      var tdElement = document.createElement('td');
+      tdElement.textContent = this.cookiesSoldPerHour[i];
+      rows.appendChild(tdElement);
+    }
 
-    //NEW12-13:: CREATE TABLE-data
+    //NEW:: create table-data --> add content to them --> append for totalDailySales
     tdElement = document.createElement('td');
     //GIVE TABLE-DATA CONTENTS (MIN.CUSTOMER OF EACH STORE)
-    tdElement.textContent = this.minCustomer;
-    //APPEND THE TABLE-data
+    tdElement.textContent = this.totalDailySales;
+    //APPEND table-row to THE TABLE-data
     trElement.appendChild(tdElement);
+    //append the tableRow to the TABLE
+};
+
+
+
+
+
+
+
+
 
     //NEW12-13:: CREATE TABLE-data
     tdElement = document.createElement('td');
@@ -98,49 +126,52 @@ new SalmondCookiesStore('alkiStore', 2, 16, 4.6);
     //APPEND THE TABLE-data
     trElement.appendChild(tdElement);
 
-
     //APPEN THE TABLE-ROW TO THE TABLE
-    cookiesTable.appendChild.(trElement);
+    salmon-cookies-table.appendChild.(trElement);
   };
+
 }
 
-    //
+    //****
+    //NEW 12-14-17
+    //****
 
+    function makeHeaderRow(){
+      // crate a table-rows
+      var row = document.createElement('tr');
 
+      //create table-data --> add contents --> append for the store name
+      var thEl = document.createElement('th');
+      thEl.textContent = 'Store location Name';
+      trEl.appendChilde(thEl);
 
-    //create table rows
-    //var trElement = document.createElement('tr');
-    //next: create table data
-  //  var tdElement = document.createElement('td');
-    //add content to tabe data (name of each store)
-  //  tdElement.textContent = this.customerPerHour;
-    //append the table data
-  //  trElement.appendChild(tdElement);
+    //create table-datas --> give them content --> append for hourly number
+    for(var i = 0; i < hour.length; i++) {
+      thElement = document.createElement('th');
+      thElement.textContent = hours[i];
+      trElement.appendChild(thElement);
+    }
 
+    //create table-header --> give them content --> append the daily totalDailySales
+    thElement = document.createElement('th');
+    thElement.textContent = 'ourTotals';
+    trElement.appentChild(trElement);
 
-
-
+  //NEW::append the table row to the table
+   salmon-cookies-table.appendChild(trElement);
+    }
+  makeHeaderRow();
+    for(var i = 0; i < allStores.length; i++){
+      allStores[i].render();
+    }
     salmonCookies.push(this);
   };
 }
-
-
-
-//NEW INFO:::12-13--
-function rander(min, max) {=
-
-
 //*******
-//excute actions (call functions)
+//NEW:: 12-14-17 excute actions (call functions)
 //******
-
-
 for(var i = 0; i < allSalmonCookiesStores.length; i++)
   allSalmonCookiesStores[i].render();
-
-
-
-
 
 }
 
